a41d09ff7bc7574c1896c85a406a04935dc8544d
Junior Fagundes <junim.rafael@gmail.com>
2025-03-01 01:36:02 -0300

    ajustando tabelas do bando de dados

--- database/init.sql
+++ database/init.sql
@@ -7,29 +7,30 @@ CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP, -- TIMESTAMPTZ used for time zone
-    deleted_at TIMESTAMP NULL DEFAULT NULL -- soft delete
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP -- TIMESTAMPTZ used for time zone
 );
 
 -- create category table
-CREATE TABLE IF NOT EXISTS category (
-    id_category SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS categorys (
+    id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    type transaction_type NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    id_users INT NOT NULL,
-    FOREIGN KEY (id_users) REFERENCES users(id) 
+    delete_at TIMESTAMP NULL DEFAULT NULL, -- soft delete
+    id_user INT NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES users(id) 
 );
 
 -- create transactions table
 CREATE TABLE IF NOT EXISTS transactions (
-    id_transaction SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     id_user INT NOT NULL,  
-    id_category INT NOT NULL,  
+    id_category INT NULL,  
     description VARCHAR(255),  
-    value DECIMAL(10, 2) NOT NULL,  
-    type transaction_type NOT NULL,  
+    value DECIMAL(10, 2) NOT NULL,
+    type transaction_type NOT NULL, 
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP, 
     date_transaction DATE NOT NULL, 
+    delete_at TIMESTAMP NULL DEFAULT NULL, -- soft delete
     FOREIGN KEY (id_user) REFERENCES users(id),
-    FOREIGN KEY (id_category) REFERENCES category(id_category)
+    FOREIGN KEY (id_category) REFERENCES categorys(id) ON DELETE SET NULL
 );
