e3bde1bd38f2f9b4d44a965d7dcae862bc97df31
Junior Fagundes <junim.rafael@gmail.com>
2025-03-02 03:04:08 -0300

    criação de usuário

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
